1e0d07173760c4c986d01ded25ac72b3245c0b17
basit <basitng2004@gmail.com>
2022-01-23 18:28:49 -0800

    made changes in cart

--- src/modals/Cart.js
+++ src/modals/Cart.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     overflowX: "hidden",
     position: "relative",
     [theme.breakpoints.down("xs")]: {
-      width: "50%",
+      width: "70%",
       padding: 10,
       background: "red",
     },
